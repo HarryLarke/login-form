7fd1461163ec750e4eead75d732f442e9a032bd7
HarryLarke <harrylarke1@hotmail.com>
2025-05-07 21:28:33 +0100

    inerceptors

--- src/hooks/useRrefreshToken.tsx
+++ src/hooks/useRrefreshToken.tsx
@@ -1,19 +1,26 @@
 import axios from "axios";
 import useAuth from "./useAuth";
 
+
 const useRefreshToken = () => {
     const { setAuth } = useAuth()
 
     const refresh = async (): Promise<string> => {
         console.log('init useRefresh')
         const response = await axios.get<{accessToken : string, roles : string[]}>('/refresh', {
-            withCredentials: true
+            withCredentials: true,
         })
-        const accessToken = response?.data?.accessToken
-        setAuth(accessToken)
-        return accessToken
+        setAuth(prev => {
+            console.log(response?.data?.accessToken)
+            return {...prev,
+                roles: response.data.roles,
+                accessToken: response.data.accessToken
+            }
+        })
+        return response.data.accessToken
+
     }
     return refresh
 }
-
+//we are setting the state and getting a new AT returned too!
 export default useRefreshToken
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/refresh': {
+        target: 'http://localhost:3500', // your backend URL
+        changeOrigin: true,
+        secure: false
+      },
+      '/employees': {
+        target: 'http://localhost:3500',
+        changeOrigin: true,
+        secure: false
+      },
+      '/auth': {
+        target: 'http://localhost:3500',
+        changeOrigin: true,
+        secure: false
+      }
+  }}})
+
+
